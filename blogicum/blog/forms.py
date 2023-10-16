479a8bfb1263ee5d9700ed53c1793affe18248b9
Дмитрий Бойцев <dmboytsev@mail.ru>
2023-10-16 18:21:57 +0300

    try2 djangosprint4

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth import get_user_model
 
 from .models import Comment, Post, User
 
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -12,11 +12,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
+    path('auth/registration/', CreateView.as_view(
+        template_name='registration/registration_form.html',
+        form_class=UserCreationForm,
+        success_url=reverse_lazy('blog:index'),
         ),
         name='registration',
     ),
